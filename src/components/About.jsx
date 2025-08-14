8a8f5d3a9608aded1aec2b83657eddc60d8a3466
NolanBrennan611 <nolanbrennan611@gmail.com>
2025-08-13 22:19:05 -0500

    added art section and fixed url bug in css

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -16,10 +16,10 @@ const About = () => {
         })
         scrollTimeline
             .from(titleSplit.words, {
-                opacity: 0, duration: 1, yPercent: 100, ease: 'expo.out', start: 0.02
+                opacity: 0, duration: 1, yPercent: 100, ease: 'expo.out', stagger: 0.02
             })
             .from('.top-grid div, .bottom-grid div', {
-                opacity: 0, duration: 1, ease: 'expo.out', start: 0.04
+                opacity: 0, duration: 1, ease: 'expo.out', stagger: 0.04
             }, '-=0.5')
     });
     return (
